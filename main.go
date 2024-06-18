e9207ff5428a6571df44f00cf22f412da3350aa3
Dario Pellegrino <pelledarioamico@gmail.com>
2024-06-19 00:27:08 +0200

    aggiunta la stampa delle regole

--- main.go
+++ main.go
@@ -11,8 +11,8 @@ type Piastrella struct {
 	intensita int
 }
 type piano struct {
-	piastrelle *map[[2]int]*Piastrella
-	regole     []Regola
+	piastrelle map[[2]int]*Piastrella
+	regole     *[]*Regola
 }
 
 type Regola struct {
@@ -21,17 +21,25 @@ type Regola struct {
 	consumo    int
 }
 
-/*
-//TODO valurare se serve
-// restituisce un puntatore alla piastrella in posizione x, y se accesa,
-// nil altrimenti (o 0x0, 0x0 è il puntatore a nil)
-func (p piano) piastrella(x int, y int) *Piastrella {
-	return (*p.piastrelle)[pair(x, y)]
-}*/
+func (r Regola) String() string {
+	s := r.risultato + ": "
+	for a, k := range r.condizione {
+		s += fmt.Sprint(k) + a + " "
+	}
+	return s
+}
 
-//(utility per inesistenza di un pair in go)
-//restituisce un vettore di 2 posizioni (pair) contenente
-//le coordinate x e y
+// (utility per questione di leggibilità)
+// restituisce un puntatore alla piastrella in posizione x, y e false (no errore) se accesa,
+// nil e true altrimenti (o 0x0, 0x0 è il puntatore a nil)
+func (p piano) piastrella(x int, y int) (*Piastrella, bool) {
+	ps, e := (p.piastrelle)[pair(x, y)]
+	return ps, e
+}
+
+// (utility per inesistenza di un pair in go)
+// restituisce un vettore di 2 posizioni (pair) contenente
+// le coordinate x e y
 func pair(x, y int) [2]int {
 	return [2]int{x, y}
 }
@@ -39,24 +47,44 @@ func pair(x, y int) [2]int {
 // colora una piastrella in posizione (x,y) sul piano p
 // con colore alpha e intensità i
 func colora(p piano, x int, y int, alpha string, i int) {
-	(*p.piastrelle)[pair(x, y)] = &Piastrella{colore: alpha, intensita: i}
+	(p.piastrelle)[pair(x, y)] = &Piastrella{colore: alpha, intensita: i}
 }
 
 // spegne la piastrella se accesa, non fa niente altrimenti
 func spegni(p piano, x int, y int) {
-	delete(*p.piastrelle, pair(x, y))
+	delete(p.piastrelle, pair(x, y))
 }
 
 // stampa in console lo stato di una piastrella nella forma "colore intensità"
 // e restituisce i due valore sottoforma di stringa e intero
 // non fa niente se la piastrella è spenta e restituisce una stringa vuota e zero
 func stato(p piano, x int, y int) (string, int) {
-	piastrella, err := (*p.piastrelle)[[2]int{x, y}]
+	piastrella, err := p.piastrella(x, y)
 	if err {
 		fmt.Printf("%s %d\n", piastrella.colore, piastrella.intensita)
 		return piastrella.colore, piastrella.intensita
 	}
 	return "", 0
+}
+
+// aggiunge una regola
+func regola(p piano, regola string) {
+	tokens := strings.Split(regola, " ")
+	condizione := make(map[string]int)
+	for i := 1; i < len(tokens)-1; i += 2 {
+		// è per forza 1 digit assumendo input corretto
+		condizione[tokens[i+1]] = int(tokens[i][0] - '0')
+	}
+	*p.regole = append(*(p.regole), &Regola{condizione: condizione, risultato: tokens[0]})
+}
+
+// stampa le regole di propagazione nell'ordine attuale
+func stampa(p piano) {
+	fmt.Println("(")
+	for _, regola := range *p.regole {
+		fmt.Println(*regola)
+	}
+	fmt.Println(")")
 }
 
 func esegui(p piano, s string) {
@@ -77,12 +105,17 @@ func esegui(p piano, s string) {
 		x, _ := strconv.Atoi(tokens[0])
 		y, _ := strconv.Atoi(tokens[1])
 		stato(p, x, y)
+	case "r":
+		regola(p, s[2:])
+	case "s":
+		stampa(p)
 	}
 }
 
 func creaPiano() piano {
 	p := make(map[[2]int]*Piastrella)
-	return piano{&p, []Regola{}}
+	r := make([]*Regola, 0)
+	return piano{p, &r}
 }
 
 func main() {
@@ -92,4 +125,7 @@ func main() {
 	esegui(p, "C 1 1 b 5")
 	esegui(p, "? 0 2")
 	esegui(p, "? 1 1")
+	esegui(p, "r a 1 a 2 b 1 c")
+	esegui(p, "r b 1 a 3 c")
+	esegui(p, "s")
 }
